e73b568a0c2732f44f0ffab533b1c22e7c0e2da2
Leandro Martins <lmarttinssilva@gmail.com>
2019-12-28 21:57:17 -0300

    Update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,4 @@ FROM ambientum/php:7.3-nginx
 WORKDIR /var/www/app
 ADD . /var/www/app
 RUN composer install
+RUN composer dump
--- app/Application/Command.php
+++ app/Application/Command.php
@@ -4,7 +4,7 @@ namespace Lms\Cashback\Application;
 
 /**
  * Interface Command
- * 
+ *
  * @package Lms\Cashback\Application
  */
 interface Command
